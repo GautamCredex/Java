851548cde27428ad32963ed2f76f6ed4b6cc8881
AbhishekGautam <abhishek.gautam@credextechnology.com>
2023-01-27 23:15:43 +0530

    new subtract added

---
+++ MathSub.java
@@ -0,0 +1,17 @@
+public class MathSub{
+    public static int add(int a, int b){
+        return a+b;
+
+    }
+    public static int sub(int a, int b){
+        return a-b;
+
+    }
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 5;
+        add(a,b);
+        sub(a,b);
+        
+    }
+}
