c758a5f735f14b83ae0d8efbc3de39c2254b4721
AbhishekGautam <abhishek.gautam@credextechnology.com>
2023-01-27 22:55:44 +0530

    new file added

---
+++ MathProblem.java
@@ -0,0 +1,12 @@
+public class MathProblem{
+    public static int add(int a, int b){
+        return a+b;
+
+    }
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 8;
+        add(a,b);
+        
+    }
+}
