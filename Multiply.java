ef59d60cdeaecaa82c2e6dc1f965ffdd9f5a0e8c
AbhishekGautam <abhishek.gautam@credextechnology.com>
2023-01-29 22:42:41 +0530

    new multiply feature added

---
+++ Multiply.java
@@ -0,0 +1,14 @@
+public class Multiply {
+    public static int mult(int a, int b){
+        return a*b;
+
+    }
+   
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 5;
+        mult(a,b);
+        
+    }
+    
+}
