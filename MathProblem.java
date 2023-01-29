502dcf26c8b8bf71d520712de59332a4f4d4e18e
AbhishekGautam <abhishek.gautam@credextechnology.com>
2023-01-29 23:38:08 +0530

    new feature upgraded

--- MathProblem.java
+++ MathProblem.java
@@ -3,10 +3,21 @@ public class MathProblem{
         return a+b;
 
     }
+    public static int divide(int a, int b){
+	    return a/b;
+    }
+     public static int multiply(int a, int b){
+            return a*b;
+    }
+
     public static void main(String[] args) {
         int a = 7;
         int b = 8;
         add(a,b);
+	divide(a,b);
+	multiply(a,b);
         
     }
+
 }
+
